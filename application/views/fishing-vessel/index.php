b187d0437cf9d70331e883dff676ae1d42587248
70ni5m <satanatu@hotmail.com>
2018-05-04 14:09:50 +0700

    สร้างไว้เชื่อมต่อข้อมูลเรือประมง

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -1,38 +1,19 @@
 <div class="row">
+<?php
+foreach($vessels as $ship):
+?>
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
         <div class="card">
-        <h5 class="card-header">ชื่อเรือ</h5>
+        <h5 class="card-header"></h5>
+            <?php echo $ship['Name']?>
             <div class="card-body">
-            <h5 class="card-title">ประเทศ</h5>
+            <h5 class="card-title"></h5>
+            <?php echo $ship['Country_ID']?>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
     </div>
 </div>
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-        <h5 class="card-header">ชื่อเรือ</h5>
-            <div class="card-body">
-            <h5 class="card-title">ประเทศ</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-        <h5 class="card-header">ชื่อเรือ</h5>
-            <div class="card-body">
-            <h5 class="card-title">ประเทศ</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-        <h5 class="card-header">ชื่อเรือ</h5>
-            <div class="card-body">
-            <h5 class="card-title">ประเทศ</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
+<?php
+    endforeach
+?>
 </div>
