c89e83ec0740efc17a3461ce2c60a635b2ba68eb
70ni5m <satanatu@hotmail.com>
2018-05-04 10:45:34 +0700

    เพิ่ม card ละ class ตามขนาดหน้าจอต่างๆ

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -26,4 +26,13 @@
             </div>
         </div>
     </div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+        <h5 class="card-header">ชื่อเรือ</h5>
+            <div class="card-body">
+            <h5 class="card-title">ประเทศ</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
 </div>
