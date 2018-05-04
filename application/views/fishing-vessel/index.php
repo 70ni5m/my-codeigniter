03aa782e8d34ad58674daf074b3c531840e7e282
70ni5m <satanatu@hotmail.com>
2018-05-04 10:25:26 +0700

    ทำ card หน้า index

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -1,5 +1,29 @@
 <div class="row">
-    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: blue"></div>
-    <h1 class="col-lg-4 col-sm-12 col-md-10">Hello, Fishers!</h1>
-    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: green"></div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+        <h5 class="card-header">ชื่อเรือ</h5>
+            <div class="card-body">
+            <h5 class="card-title">ประเทศ</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+    </div>
+</div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+        <h5 class="card-header">ชื่อเรือ</h5>
+            <div class="card-body">
+            <h5 class="card-title">ประเทศ</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+        <h5 class="card-header">ชื่อเรือ</h5>
+            <div class="card-body">
+            <h5 class="card-title">ประเทศ</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
 </div>
