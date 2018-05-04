b5fd512da33e697b9bf9c3634d9167bb7237c425
70ni5m <satanatu@hotmail.com>
2018-05-04 15:16:20 +0700

    เพิ่มรายชื่อประเทศลงใน form

--- application/controllers/FishingVessel.php
+++ application/controllers/FishingVessel.php
@@ -12,8 +12,9 @@ class FishingVessel extends CI_Controller {
         $result = $this->fishingvessel_model->get_all();
 
         $data['vessels'] = $result;
+        $data['title'] = "รายชื่อเรือประมงทั้งหมด";
 
-        $this->load->view('header');
+        $this->load->view('header', $data);
         $this->load->view('fishing-vessel/index', $data);
         $this->load->view('footer');       
     }
@@ -25,8 +26,14 @@ class FishingVessel extends CI_Controller {
 
     public function new_vessel()
     {
-        $this->load->view('header');
-        $this->load->view('fishing-vessel/new-vessel');
+        $this->load->model('country_model');
+        $result = $this->country_model->get_all();
+
+        $data['country_list'] = $result;
+        $data['title'] = "เพิ่มข้อมูลเรือประมง";
+        
+        $this->load->view('header', $data);
+        $this->load->view('fishing-vessel/new-vessel', $data);
         $this->load->view('footer');
     }
 
