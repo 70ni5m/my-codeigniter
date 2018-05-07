c8ae6d80a83a47d1c4bf2ac1a63cf760d25fc59c
70ni5m <satanatu@hotmail.com>
2018-05-07 14:25:00 +0700

    ตั้งค่าสำหรับ  upload file สั่งอัพโหลดไฟล์จาก validate file ผ่านแล้ว

--- application/controllers/FishingVessel.php
+++ application/controllers/FishingVessel.php
@@ -40,7 +40,11 @@ class FishingVessel extends CI_Controller {
     public function create()
     {
 
-        // เดี๋ยวจะใส่ code upload ไฟล์
+        $config['upload_path'] = './upload/';
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        // $config['max_size'] = 100;
+        // $config['max_width'] = 1024;
+        $this->load->library('upload', $config);
 
         $this->load->library('form_validation');
         $this->form_validation->set_rules('vesselName', 'ชื่อเรือ', 'required|max_length[10]', array('required' => 'ช่วยกรอกชื่อสำเภาด้วยนะออเจ้า'));
@@ -58,13 +62,36 @@ class FishingVessel extends CI_Controller {
         $this->load->view('footer');
         }
 
-        else{
-            $this->load->model('fishingvessel_model');
-         
-            $this->fishingvessel_model->save_new_vessel();
+        else
+        {
+            $upload_success = $this->upload->do_upload('vesselImage');
 
+            if($upload_success)
+            {
+                $upload_data = $this->upload->data();
+                
+                // var_dump($upload_data);
+
+                $image_path = 'Upload/' . $upload_data['file_name'];
+
+                // $this->load->model('fishingvessel_model');
+                // $this->fishingvessel_model->save_new_vessel();
+                // redirect('fishingvessel/');
+            }
+
+            else
+            {
+                $this->load->model('country_model');
+                $result = $this->country_model->get_all();
         
-            redirect('fishingvessel','refresh');
+                $data['country_list'] = $result;
+                $data['title'] = "เพิ่มข้อมูลเรือประมง";
+                
+                $this->load->view('header', $data);
+                $this->load->view('fishing-vessel/new-vessel', $data);
+                $this->load->view('footer');
+            }
+           
         }
     }
 
