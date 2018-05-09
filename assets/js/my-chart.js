3b662e27949e345dc83ed781494b92e00fc53b20
70ni5m <satanatu@hotmail.com>
2018-05-09 15:45:33 +0700

    เพิ่ม web api สำหรับแสดงข้อมูลเรือทุกลำเป็น json

--- assets/js/my-chart.js
+++ assets/js/my-chart.js
@@ -1,6 +1,33 @@
 
-$(document).ready(function()){
+$(document).ready(function(){
 
-    alert('ok');
+    $('#bar-chart').insertFusionCharts({
+        type: 'column2d',
+        width: "100%",
+        hight: "400",
+        dataFormat: "json",
+        dataSource: {
+            "chart":{
+                "caption": "จำนวนเรือประมงแบ่งตามประเทศ",
+                "xAxisName": "ประเทศ",
+                "yAxisName": "จำนวน/ลำ",
+                "theme": "fint"
+            },
+            "data":[
+                {
+                "label": "ประเทศไทย",
+                "value": "120"
+                },
+                {
+                "label": "บรูไน",
+                "value": "230",    
+                },
+                {
+                "label": "กัมพูชา",
+                "value": "400"
+                }
+            ]
+        }
+    });
 
 });
